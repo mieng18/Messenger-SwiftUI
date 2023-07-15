33e833d0dcb57284e68d646fe7b3d7437a042fd0
mieng18 <mieng18@hotmail.com>
2023-07-15 00:07:29 -0700

    Initial Commit

---
+++ Messenger-SwiftUI/Messenger_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_SwiftUIApp.swift
+//  Messenger-SwiftUI
+//
+//  Created by mai nguyen on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
